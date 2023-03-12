a73410ddebf37f65f7d9aaffcc866973b01dfefc
murray <murray.watts@lastyard.com>
2023-03-12 13:01:29 +0800

    Test

--- app/server.go
+++ app/server.go
@@ -26,26 +26,26 @@ func main() {
 	}()
 
 	for {
-		fmt.Println("Waiting for connection")
-		connection, err := l.Accept()
+		fmt.Println("Waiting for conn")
+		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Println("Error accepting conn: ", err.Error())
 			os.Exit(1)
 		}
+		fmt.Println("Connection accepted")
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			fmt.Println("Error reading from conn: ", err.Error())
 			os.Exit(1)
 		}
-		_, err = connection.Write([]byte("+PONG\r\n"))
+		fmt.Println("Successfully read from conn")
+		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
+			fmt.Println("Error writing to conn: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("Successfully wrote to connection")
-		err = connection.Close()
-		if err != nil {
-			fmt.Println("Error closing connection: ", err.Error())
-			os.Exit(1)
-		}
+		fmt.Println("Successfully wrote to conn")
+		conn.Close()
 	}
 }
