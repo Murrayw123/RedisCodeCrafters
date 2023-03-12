ab694eda7c7d19213407e89308488dbbf1a1a27e
murray <murray.watts@lastyard.com>
2023-03-13 05:54:36 +0800

    remove the connection close

--- app/server.go
+++ app/server.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
 	if err != nil {
@@ -26,7 +23,7 @@ func main() {
 	}()
 
 	for {
-		fmt.Println("Waiting for conn")
+		fmt.Println("Waiting for connection")
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting conn: ", err.Error())
@@ -39,6 +36,12 @@ func main() {
 			fmt.Println("Error reading from conn: ", err.Error())
 			os.Exit(1)
 		}
+
+		fmt.Println("Read from conn: ", string(buf))
+		if err != nil {
+			fmt.Println("Error reading from conn: ", err.Error())
+			os.Exit(1)
+		}
 		fmt.Println("Successfully read from conn")
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
@@ -46,6 +49,5 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println("Successfully wrote to conn")
-		conn.Close()
 	}
 }
