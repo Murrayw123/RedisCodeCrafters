aaf42aa52c855c88e92ed8c954406453056d760a
murray <murray.watts@lastyard.com>
2023-03-12 12:41:52 +0800

    broken

--- app/server.go
+++ app/server.go
@@ -16,7 +16,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
+	defer func() {
+		err := l.Close()
+		if err != nil {
+			fmt.Println("Error closing connection: ", err.Error())
+			os.Exit(1)
+		}
+	}()
+
 	for {
+		fmt.Println("Waiting for connection")
 		connection, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
