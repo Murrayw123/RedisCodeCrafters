971e2a16128fcf6dc73aede3560fe6df9fd83b1a
murray <murray.watts@lastyard.com>
2023-03-19 08:19:37 +0800

    handle concurrent clients

--- app/server.go
+++ app/server.go
@@ -7,6 +7,25 @@ import (
 	"os"
 )
 
+func handleRequest(conn net.Conn) {
+	for {
+		buf := make([]byte, 1024)
+
+		if _, err := conn.Read(buf); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("error reading from client: ", err.Error())
+				os.Exit(1)
+			}
+		}
+
+		// Let's ignore the client's input for now and hardcode a response.
+		// We'll implement a proper Redis Protocol parser in later stages.
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 
@@ -24,22 +43,15 @@ func main() {
 	}()
 
 	fmt.Println("Waiting for connection")
-	conn, err := l.Accept()
 
 	for {
-		buf := make([]byte, 1024)
-
-		if _, err := conn.Read(buf); err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println("error reading from client: ", err.Error())
-				os.Exit(1)
-			}
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
+		go handleRequest(conn)
 
-		// Let's ignore the client's input for now and hardcode a response.
-		// We'll implement a proper Redis Protocol parser in later stages.
-		conn.Write([]byte("+PONG\r\n"))
 	}
+
 }
